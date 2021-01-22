864bb32b4f32f3a82eee9b412c7c0b1fa283a0ad
Vitor Veras <vitorveras01@gmail.com>
2021-01-22 19:29:38 -0300

    Fifth Lecture (Creating first data (C)) - Config of creating phase (C) of crud - Usage of arrays in wire model by typing the property

--- app/Http/Livewire/Book/Create.php
+++ app/Http/Livewire/Book/Create.php
@@ -3,9 +3,38 @@
 namespace App\Http\Livewire\Book;
 
 use Livewire\Component;
+use App\Models\Book;
 
 class Create extends Component
 {
+    /**
+     * This property will be an Book type
+     */
+    public Book $book;
+
+    /**
+     * Its necessary to specify each validation of fillable!
+     */
+    protected $rules = [
+        'book.name' => 'required',
+        'book.pages' => 'required|integer',
+        'book.author' => 'required|string',
+    ];
+
+    /**
+     * To use $book its necessary to initialize this property 
+     */
+    public function mount()
+    {
+        $this->book = new Book();
+    }
+
+    public function save()
+    {
+        Book::create($this->book->toArray());
+        return redirect()->route('books.index');
+    }
+
     public function render()
     {
         return view('livewire.book.create');
--- resources/views/livewire/book/create.blade.php
+++ resources/views/livewire/book/create.blade.php
@@ -1,3 +1,26 @@
 <div>
-    <p>Create Component from livewire</p>
+    <form wire:submit.prevent="save" class="w-full">
+        <div class="w-full px-3 mb-6 md:mb-0">
+            <label for="name" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Nome
+            </label>
+            <!-- Using book. informs that book will be a array -->
+            <input wire:model="book.name" type="text" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="name">
+        </div>
+        <div class="w-full px-3 mb-6 md:mb-0">
+            <label for="pages" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Pages number
+            </label>
+            <!-- Using book. informs that book will be a array -->
+            <input wire:model="book.pages" type="number" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="pages">
+        </div>
+        <div class="w-full px-3 mb-6 md:mb-0">
+            <label for="author" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Author
+            </label>
+            <!-- Using book. informs that book will be a array -->
+            <input wire:model="book.author" type="text" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="author">
+        </div>
+        <button type="submit" >Register</button>
+    </form>
 </div>
